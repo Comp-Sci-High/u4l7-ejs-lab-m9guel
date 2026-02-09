4090a59234030675f195fd9e597e070c6bc962c1
m9guel <miguel.sanchez26@compscihigh.org>
2026-02-09 20:21:01 +0000

    test

--- index.js
+++ index.js
@@ -75,19 +75,19 @@ app.use((req, res, next) => {
 
 // Task 1: Set up the static middleware
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + '/public'))
 
 // Task 2: Set up the route handler for / to send back the index.html file
 /
 app.get('/',(req,res)=>{
-res.render('index.html')
+res.sendFile(__dirname + '/public/index.html')
 
 })
 
 // Task 3: Set up the route handler for /mens which sends back category.ejs with the men's category object
 
 app.get('/mens',(req,res)=>{
-res.render('category.ejs', inventory[0].category)
+res.render('category.ejs', inventory[0])
 
 })
 
@@ -96,14 +96,14 @@ res.render('category.ejs', inventory[0].category)
 // Task 4: Plug in the values in category.ejs to get the page working
 // PINK ONLY: Set up a route handler for /womens to pass in similar data for women's
 app.get('/womens',(req,res)=>{
-res.send('category.ejs', inventory[1].category)
+res.send('category.ejs', inventory[1])
 
 })
 
 
 // Task 5: Set up the route handler for /item/0 which sends back the first item in product.ejs
 app.get('/item/0',(req,res)=>{
-res.send('product.ejs', inventory[0].items[0])
+res.send('product.ejs', inventory[0].items)
 
 })
 
